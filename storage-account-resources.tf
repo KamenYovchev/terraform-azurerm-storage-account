102ad522b6ff78f75fb2b558ae33d84c08e2c013
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-10-13 18:43:27 +0300

    add new resources

--- storage-account-resources.tf
+++ storage-account-resources.tf
@@ -22,3 +22,59 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = var.replication_type
 }
 
+resource "azurerm_storage_container" "ct" {
+  name                 = "terraform-states"
+  storage_account_name = azurerm_storage_account.sa.name
+
+}
+
+data "azurerm_storage_account_sas" "state" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  https_only        = true
+
+  resource_types {
+    service   = true
+    container = true
+    object    = true
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = timestamp()
+  expiry = timeadd(timestamp(), "17520h")
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = true
+    list    = true
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+}
+
+#############################################################################
+# PROVISIONERS
+#############################################################################
+
+resource "null_resource" "post-config" {
+
+  depends_on = [azurerm_storage_container.ct]
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo 'storage_account_name = "${azurerm_storage_account.sa.name}"' >> backend-config.txt
+echo 'container_name = "terraform-state"' >> backend-config.txt
+echo 'key = "devops.tfstate"' >> backend-config.txt
+echo 'sas_token = "${data.azurerm_storage_account_sas.state.sas}"' >> backend-config.txt
+EOT
+  }
+}
+

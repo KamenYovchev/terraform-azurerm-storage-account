7d81bac3dd4803e9686d1dfa3adf26147730f2ac
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-10-13 18:23:40 +0300

    change resource in main.tf

--- storage-account-resources.tf
+++ storage-account-resources.tf
@@ -14,7 +14,8 @@ resource "random_integer" "sa_num" {
 
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "${lower(var.name)}${random_integer.sa_num.result}"
+  #name                     = "${lower(var.name)}${random_integer.sa_num.result}"
+  name                     = lower(var.name)
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
